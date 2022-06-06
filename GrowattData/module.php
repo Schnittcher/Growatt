95c822b7c374f7aa7862724e241b57e7b0b26960
Kai Schnittcher <kai.schnittcher@gmail.com>
2022-06-06 12:50:02 +0200

    Fix für SymBox

--- GrowattData/module.php
+++ GrowattData/module.php
@@ -2,6 +2,61 @@
 
 declare(strict_types=1);
 
+if (!function_exists('fnmatch')) {
+    define('FNM_PATHNAME', 1);
+    define('FNM_NOESCAPE', 2);
+    define('FNM_PERIOD', 4);
+    define('FNM_CASEFOLD', 16);
+
+    function fnmatch($pattern, $string, $flags = 0)
+    {
+        return pcre_fnmatch($pattern, $string, $flags);
+    }
+}
+
+function pcre_fnmatch($pattern, $string, $flags = 0)
+{
+    $modifiers = null;
+    $transforms = [
+        '\*'      => '.*',
+        '\?'      => '.',
+        '\[\!'    => '[^',
+        '\['      => '[',
+        '\]'      => ']',
+        '\.'      => '\.',
+        '\\'      => '\\\\'
+    ];
+
+    // Forward slash in string must be in pattern:
+    if ($flags & FNM_PATHNAME) {
+        $transforms['\*'] = '[^/]*';
+    }
+
+    // Back slash should not be escaped:
+    if ($flags & FNM_NOESCAPE) {
+        unset($transforms['\\']);
+    }
+
+    // Perform case insensitive match:
+    if ($flags & FNM_CASEFOLD) {
+        $modifiers .= 'i';
+    }
+
+    // Period at start must be the same as pattern:
+    if ($flags & FNM_PERIOD) {
+        if (strpos($string, '.') === 0 && strpos($pattern, '.') !== 0) {
+            return false;
+        }
+    }
+
+    $pattern = '#^'
+        . strtr(preg_quote($pattern, '#'), $transforms)
+        . '$#'
+        . $modifiers;
+
+    return (boolean) preg_match($pattern, $string);
+}
+
 require_once __DIR__ . '/../libs/vendor/SymconModulHelper/VariableProfileHelper.php';
 
     class GrowattData extends IPSModule
@@ -134,7 +189,7 @@ require_once __DIR__ . '/../libs/vendor/SymconModulHelper/VariableProfileHelper.
             $this->SendDebug('JSON', $JSONString, 0);
             if (!empty($this->ReadPropertyString('MQTTTopic'))) {
                 $data = json_decode($JSONString, true);
-                if (array_key_exists('Topic',$data)) {
+                if (array_key_exists('Topic', $data)) {
                     if (fnmatch('energy/growatt', $data['Topic'])) {
                         $this->SendDebug('Payload', $data['Payload'], 0);
                         $Payload = json_decode($data['Payload'], true);
