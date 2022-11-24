81377e5622a3f7df004a46a251ddf72b5dd0b11f
Kai Schnittcher <kai.schnittcher@gmail.com>
2022-11-24 10:39:24 +0100

    38,39,42,43,46,47,50,51,52 hinzugefügt

--- GrowattTL3-X/module.php
+++ GrowattTL3-X/module.php
@@ -7,7 +7,6 @@ require_once __DIR__ . '/../libs/GrowattModBus.php';
     class GrowattTL3X extends GrowattModBus
     {
         const PREFIX = 'GWTL3X';
-
         public static $Variables = [
             ['Output Power', VARIABLETYPE_FLOAT, '~Watt', 0.1,  0x0024, 4, 1,  null, true],
             ['Input Power', VARIABLETYPE_FLOAT, '~Watt', 0.1,  0x0002, 4, 1,  null, true],
@@ -30,7 +29,16 @@ require_once __DIR__ . '/../libs/GrowattModBus.php';
             ['Inverter Status', VARIABLETYPE_INTEGER, 'Growatt.Status', null,  0x0000, 4, 1,  null, true],
             ['Inverter Temperature', VARIABLETYPE_FLOAT, '~Temperature', 0.1,  0x5D, 4, 1,  null, true],
             ['Active Power Rate', VARIABLETYPE_INTEGER, '~Intensity.100', null,  0x0003, 3, 1,  6, true],
-            ['Memory State', VARIABLETYPE_INTEGER, 'Growatt.MemoryState', null,  0x0002, 3, 1,  6, true]
+            ['Memory State', VARIABLETYPE_INTEGER, 'Growatt.MemoryState', null,  0x0002, 3, 1,  6, true],
+            ['Phase 1 Grid Voltage', VARIABLETYPE_FLOAT, '~Volt', 0.1,  0x0026, 4, 1,  null, true],
+            ['Phase 1 Grid Output Current', VARIABLETYPE_FLOAT, '~Electricity', 0.1,  0x0027, 4, 1,  null, true],
+            ['Phase 2 Grid Voltage', VARIABLETYPE_FLOAT, '~Volt', 0.1,  0x002A, 4, 1,  null, true],
+            ['Phase 2 Grid Output Current', VARIABLETYPE_FLOAT, '~Electricity', 0.1,  0x002B, 4, 1,  null, true],
+            ['Phase 3 Grid Voltage', VARIABLETYPE_FLOAT, '~Volt', 0.1,  0x002E, 4, 1,  null, true],
+            ['Phase 3 Grid Output Current', VARIABLETYPE_FLOAT, '~Electricity', 0.1,  0x002F, 4, 1,  null, true],
+            ['L1 L2 Voltage', VARIABLETYPE_FLOAT, '~Volt', 0.1,  0x0032, 4, 1,  null, true],
+            ['L2 L3 Voltage', VARIABLETYPE_FLOAT, '~Volt', 0.1,  0x0033, 4, 1,  null, true],
+            ['L3 L1 Voltage', VARIABLETYPE_FLOAT, '~Volt', 0.1,  0x0034, 4, 1,  null, true]
         ];
 
         public function Destroy()
